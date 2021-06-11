a59814a65d784e74711134d6e544f0594476b3ee
BuildTools <unconfigured@null.spigotmc.org>
2021-06-11 16:15:29 +0800

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '83455d5b41ca850746d1c477c11c8531'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1bac26ed-9103-43fc-835d-3fe5f152821c'
+};
